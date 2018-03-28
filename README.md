b590b35e120048d7d2d017e40542057dfc5bfb26
codediodeio <hello@codediode.io>
2018-03-27 17:02:20 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Episode XY
+
+Watch the [screencast](https://angularfirebase.com/lessons)
+
+## Usage
+
+- `git clone`
+- create the `src/enviornments/environment.ts` file and add your firebase config to it
+- `npm install`
+- `ng serve`
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
+
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+
+// Firebase setup instructions
+
+// 1. delete this line, then...
+import { firebaseConfig } from '../env'; 
+
+// 2. Add your own firebase config to environment.ts
+// 3. Use the environment to initialize angularfire2 below, like so AngularFireModule.initializeApp(environment.firebaseConfig),
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule,
+    AngularFireAuthModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
